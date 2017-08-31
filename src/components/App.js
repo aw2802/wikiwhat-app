44de1c79c3fc4cfb4382970416a7f9cfca188a80
jrg4017 <jrg4017@rit.edu>
2017-08-31 10:27:30 -0400

    update SASS file to match those in wikiwhat-storybook

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import axios from 'axios';
+
 import Navbar from './Navbar';
 import Selection from './Selection';
-import axios from 'axios';
-import '../css/App.css';
 
+import { ApolloClient, ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient();
 class App extends React.Component{
 
 	constructor(){
@@ -11,31 +14,21 @@ class App extends React.Component{
 		this.state={images : []};
 	}
 
-	componentWillMount(){
-		let config = {'X-Mashape-Key': 'ZF4AQocD2Cmsh9IU2WEtKtDHIYrjp16SwqzjsnqF9PbncTEYs7',
-					   'Accept': 'application/json'
-		};
-		var th = this;
-    	this.serverRequest = 
-      	axios.get('https://hargrimm-wikihow-v1.p.mashape.com/images?count=4', {headers: config})
-        .then(function(result) {    
-          th.setState({
-            images: result.data
-          });
-        })
-	}
+
 
 	render(){
 		var obj = JSON.stringify(this.state.images);
 		obj = obj.split("\"");
 
 	    return (
-	      <div className='App'>
-	        <Navbar />
-	        <img src={obj[3]} />
-	    	{/* Uses the first image as the main image */}
-	        <Selection/>
-	      </div>
+	      <ApolloProvider client={client}>
+					<div className='App'>
+		        <Navbar />
+		        <img src={obj[3]} />
+		    	{/* Uses the first image as the main image */}
+		        <Selection/>
+		      </div>
+				</ApolloProvider>
 	    )
 	}
 }
